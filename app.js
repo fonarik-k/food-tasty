7aaef04412d7b05c756c87d8598ac9217d801922
fonarik-k <prosto87879@gmail.com>
2026-02-11 12:11:43 +0500

    1

---
+++ app.js
@@ -0,0 +1,109 @@
+let fastFood = [
+    {
+        categories: [
+            {
+                categoriyName: 'Salads',
+                categoriyImage: 'https://simple-veganista.com/wp-content/uploads/2012/09/healthy-chopped-vegetable-salad-recipe-3.jpg',
+                categoriyPrice: 8
+            },
+            {
+                categoriyName: 'Pizza',
+                categoriyImage: 'https://upload.wikimedia.org/wikipedia/commons/9/91/Pizza-3007395.jpg',
+                categoriyPrice: 18
+            },
+            {
+                categoriyName: 'Burger',
+                categoriyImage: 'https://www.foodandwine.com/thmb/XE8ubzwObCIgMw7qJ9CsqUZocNM=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/MSG-Smash-Burger-FT-RECIPE0124-d9682401f3554ef683e24311abdf342b.jpg',
+                categoriyPrice: 12
+            },
+             {
+                categoriyName: 'Drinks',
+                categoriyImage: 'https://mestredosdrinks.com/en/wp-content/uploads/2025/08/drinks-everything-drink-lovers-need-to-know-to-make-flavorful-cocktails.jpg',
+                categoriyPrice: 4
+            },
+        ],
+        products: [
+            {
+                productName: 'Salad',
+                rating: 5.9,
+                description: 'Made with juicy beef burger, cheese...',
+                price: 9.20,
+                poster: 'https://images.immediate.co.uk/production/volatile/sites/30/2014/05/Epic-summer-salad-hub-2646e6e.jpg',
+                discountInPercentage: 30                
+            },
+            {
+                productName: 'Burger',
+                rating: 7.5,
+                description: 'Made with juicy beef burger, cheese...',
+                price: 10.70,
+                poster: 'https://images.themodernproper.com/production/posts/2016/ClassicCheeseBurger_9.jpg?w=1200&h=1200&q=60&fm=jpg&fit=crop&dm=1749310239&s=463b18fc3bb51dc5d96e866c848527c4',
+                discountInPercentage: 10                
+            },
+            {
+                productName: 'Potato',
+                rating: 5.7,
+                description: 'Made with juicy beef burger, cheese...',
+                price: 9.40,
+                poster: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbu5PprtD9AsKd4VFjLaVhcpFEsplSqYUSgQndS4iFxA&s',
+                discountInPercentage: 15              
+            },
+            {
+                productName: 'Potato',
+                rating: 4.8,
+                description: 'Made with juicy beef burger, cheese...',
+                price: 8.50,
+                poster: 'https://www.dinneratthezoo.com/wp-content/uploads/2019/12/homemade-french-fries-5.jpg',
+                discountInPercentage: 15              
+            },
+            {
+                productName: 'Salad',
+                rating: 7.8,
+                description: 'Made with juicy beef burger, cheese...',
+                price: 9.20,
+                poster: 'https://images.immediate.co.uk/production/volatile/sites/30/2014/05/Epic-summer-salad-hub-2646e6e.jpg',
+                discountInPercentage: 30                
+            }
+        ]
+    }
+]
+let container=document.querySelector('.cards')
+container.insertAdjacentHTML('beforeend',`
+   <div class="blur">
+      <div class="card" style="background-image: url(${fastFood[0].categories[0].categoriyImage});">
+              
+                    <p class="meal_name">${fastFood[0].categories[0].categoriyName}</p>
+                    <p class="meal_price">${fastFood[0].categories[0].categoriyPrice}</p>
+                    <button class="butt orange">Order now</button>
+                </div>
+                </div>
+    `)
+    container.insertAdjacentHTML('beforeend',`
+        <div class="blur">
+           <div class="card" style="background-image: url(${fastFood[0].categories[1].categoriyImage});">
+                   
+                         <p class="meal_name">${fastFood[0].categories[1].categoriyName}</p>
+                         <p class="meal_price">${fastFood[0].categories[1].categoriyPrice}</p>
+                         <button class="butt orange">Order now</button>
+                     </div>
+                     </div>
+         `)
+         container.insertAdjacentHTML('beforeend',`
+            <div class="blur">
+               <div class="card" style="background-image: url(${fastFood[0].categories[2].categoriyImage});">
+                       
+                             <p class="meal_name">${fastFood[0].categories[2].categoriyName}</p>
+                             <p class="meal_price">${fastFood[0].categories[2].categoriyPrice}</p>
+                             <button class="butt orange">Order now</button>
+                         </div>
+                         </div>
+             `)
+             container.insertAdjacentHTML('beforeend',`
+                <div class="blur">
+                   <div class="card" style="background-image: url(${fastFood[0].categories[3].categoriyImage});">
+                           
+                                 <p class="meal_name">${fastFood[0].categories[3].categoriyName}</p>
+                                 <p class="meal_price">${fastFood[0].categories[3].categoriyPrice}</p>
+                                 <button class="butt orange">Order now</button>
+                             </div>
+                             </div>
+                 `)
